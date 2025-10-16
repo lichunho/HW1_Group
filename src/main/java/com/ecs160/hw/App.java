95530940be5dd900bd9866e0f5f6b3aa6907c755
Chunho Li <lchli@ucdavis.edu>
2025-10-16 16:43:06 -0700

    Added a function skeleton for getting list of repo

--- src/main/java/com/ecs160/hw/App.java
+++ src/main/java/com/ecs160/hw/App.java
@@ -2,14 +2,19 @@ package com.ecs160.hw;
 
 import java.util.List;
 
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) {
-        
+    public static String[] getRepoList(String language) {
+        String[] repoList = new String[10];
+        return repoList;
     }
+
+    public static void main( String[] args ) {
+        System.out.println("Hello, World!");
+    }
+    
 }
